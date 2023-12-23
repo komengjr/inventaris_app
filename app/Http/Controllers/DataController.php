fb60cb8a4403f492efc95e2f5aa4d729b207e4b0
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2023-12-23 12:00:40 +0700

    updatelaporan21

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -17,7 +17,7 @@ class DataController extends Controller
         $data = DB::table('sub_tbl_inventory')->join('tbl_lokasi','tbl_lokasi.kd_lokasi','=','sub_tbl_inventory.kd_lokasi')->where('sub_tbl_inventory.no_inventaris',$request->data)->get();
         return view('tampil',['data'=>$data]);
     }
-    public function scandataineventaris(Request $request, $id)
+    public function scandataineventaris(Request $request,$id)
     {
         $data = DB::table('sub_tbl_inventory')->join('tbl_lokasi','tbl_lokasi.kd_lokasi','=','sub_tbl_inventory.kd_lokasi')->where('sub_tbl_inventory.no_inventaris',$request->data)->first();
         return view('divisi.stockopname.inputverif',['data'=>$data]);
--- resources/views/divisi/stockopname/inputverif.blade.php
+++ resources/views/divisi/stockopname/inputverif.blade.php
@@ -1,48 +1,53 @@
 <div class="card-body">
-    <div class="row">
-        <div class="col-md-3">
-            <label for="">Nama Barang</label>
-            <input type="text" class="form-control" value="{{$data->nama_barang}}">
+    @if ($data)
+        <div class="row">
+            <div class="col-md-3">
+                <label for="">Nama Barang</label>
+                <input type="text" class="form-control" value="{{ $data->nama_barang }}">
+            </div>
+            <div class="col-md-3">
+                <label for="">Nomor Inventaris</label>
+                <input type="text" class="form-control" value="{{ $data->no_inventaris }}">
+            </div>
         </div>
-        <div class="col-md-3">
-            <label for="">Nomor Inventaris</label>
-            <input type="text" class="form-control" value="{{$data->no_inventaris}}">
-        </div>
-    </div>
-    <!--End Row-->
+        <!--End Row-->
 
-    <hr>
-    <div class="demo-heading">Pilih Kondisi</div>
-    <div class="row">
-        <div class="col-md-4">
-            <div class="icheck-material-primary icheck-inline">
-                <input type="radio" id="inline-radio-primary" name="inlineradio" checked="">
-                <label for="inline-radio-primary">Baik</label>
+        <hr>
+        <div class="demo-heading">Pilih Kondisi</div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="icheck-material-primary icheck-inline">
+                    <input type="radio" id="inline-radio-primary" name="inlineradio" checked="">
+                    <label for="inline-radio-primary">Baik</label>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="icheck-material-info icheck-inline">
+                    <input type="radio" id="inline-radio-info" name="inlineradio">
+                    <label for="inline-radio-info">Maintenance</label>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="icheck-material-success icheck-inline">
+                    <input type="radio" id="inline-radio-success" name="inlineradio">
+                    <label for="inline-radio-success">Rusak</label>
+                </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="icheck-material-info icheck-inline">
-                <input type="radio" id="inline-radio-info" name="inlineradio">
-                <label for="inline-radio-info">Maintenance</label>
+        <div class="demo-heading">Keterangan</div>
+        <div class="row">
+            <div class="col-md-12">
+                <textarea name="" class="form-control" id="" cols="30" rows="10"></textarea>
             </div>
-        </div>
-        <div class="col-md-4">
-            <div class="icheck-material-success icheck-inline">
-                <input type="radio" id="inline-radio-success" name="inlineradio">
-                <label for="inline-radio-success">Rusak</label>
-            </div>
-        </div>
-    </div>
-    <div class="demo-heading">Keterangan</div>
-    <div class="row">
-        <div class="col-md-12">
-            <textarea name="" class="form-control" id="" cols="30" rows="10"></textarea>
-        </div>
 
-    </div>
-    <div class="row pt-2">
-        <div class="col-md-12">
-            <button class="btn-success"><i class="fa fa-save"></i> Simpan</button>
         </div>
-    </div>
+        <div class="row pt-2">
+            <div class="col-md-12">
+                <button class="btn-success"><i class="fa fa-save"></i> Simpan</button>
+            </div>
+        </div>
+    @else
+    <span class="badge badge-warning shadow-warning m-1">Barang Tidak DItemukan</span>
+    @endif
+
 </div>
