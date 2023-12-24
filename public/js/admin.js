884829ffac7df64926930a74dfcf73b6b7482bcf
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2023-12-24 15:10:34 +0700

    updatelaporan40

--- public/js/admin.js
+++ public/js/admin.js
@@ -1440,24 +1440,7 @@ $(document).on("click", "#button-print-laporan-ruangan-pdf", function (e) {
         });
 
 });
-$(document).on("click", "#button-print-laporan", function (e) {
-    e.preventDefault();
-    $("#show-data-laporan").html('<div class="spinner-border" role="status"> <span class="sr-only">Loading...</span> </div>');
-    $.ajax({
-        url: '../menureport/masterlaporan/cetak-all-barang-cabang/',
-        type: "GET",
-        dataType: "html",
-    })
-        .done(function (data) {
-            $("#show-data-laporan").html('<iframe src="data:application/pdf;base64, '+data+'" style="width:100%;; height:500px;" frameborder="0"></iframe>');
-        })
-        .fail(function () {
-            $("#show-data-laporan").html(
-                '<i class="fa fa-info-sign"></i> Something went wrong, Please try again...'
-            );
-        });
 
-});
 
 $(document).on("click", "#button-simpan-hasil-verifikasi", function(e) {
     var data = $("#form-verifikasi-data-inevntaris").serialize();
--- resources/views/divisi/laporan/view.blade.php
+++ resources/views/divisi/laporan/view.blade.php
@@ -1,15 +1,37 @@
 <div class="modal-content">
     <div class="modal-header">
-      <h5 class="modal-title">Report</h5>
-      <button type="button" class="btn-danger" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">×</span>
-      </button>
+        <h5 class="modal-title">Report</h5>
+        <button type="button" class="btn-danger" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
     </div>
     <div class="modal-body" id="show-data-laporan">
-      <img src="https://via.placeholder.com/800x500" class="img-fluid rounded shadow" alt="Card image cap">
+        <img src="https://via.placeholder.com/800x500" class="img-fluid rounded shadow" alt="Card image cap">
     </div>
     <div class="modal-footer">
-      <button type="button" class="btn-dark" data-dismiss="modal"><i class="fa fa-times"></i> EXCEL</button>
-      <button type="button" class="btn-success" id="button-print-laporan"><i class="fa fa-print"></i> PDF</button>
+        <button type="button" class="btn-dark" data-dismiss="modal"><i class="fa fa-times"></i> EXCEL</button>
+        <button type="button" class="btn-success" id="button-print-laporan"><i class="fa fa-print"></i> PDF</button>
     </div>
-  </div>
+</div>
+<script>
+    $(document).on("click", "#button-print-laporan", function(e) {
+        e.preventDefault();
+        $("#show-data-laporan").html(
+            '<div class="spinner-border" role="status"> <span class="sr-only">Loading...</span> </div>');
+        $.ajax({
+                url: '../menureport/masterlaporan/cetak-all-barang-cabang/',
+                type: "GET",
+                dataType: "html",
+            })
+            .done(function(data) {
+                $("#show-data-laporan").html('<iframe src="data:application/pdf;base64, ' + data +
+                    '" style="width:100%;; height:500px;" frameborder="0"></iframe>');
+            })
+            .fail(function() {
+                $("#show-data-laporan").html(
+                    '<i class="fa fa-info-sign"></i> Something went wrong, Please try again...'
+                );
+            });
+
+    });
+</script>
