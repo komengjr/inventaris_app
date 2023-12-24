40ff32d1719731f6639dd34cd2ecc94dae0d7323
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2023-12-24 13:17:22 +0700

    updatelaporan27

--- resources/views/divisi/laporan/report/per-ruangan.blade.php
+++ resources/views/divisi/laporan/report/per-ruangan.blade.php
@@ -149,7 +149,7 @@
                 </tr>
             </thead>
             <tbody>
-                @php
+                {{-- @php
                     $no = 1;
                 @endphp
                 @foreach ($data as $data)
@@ -162,7 +162,7 @@
                         <td style="text-align: right;">@currency($data->harga_perolehan)</td>
                         <td>Baik</td>
                     </tr>
-                @endforeach
+                @endforeach --}}
             </tbody>
         </table>
         <br><br><br>
