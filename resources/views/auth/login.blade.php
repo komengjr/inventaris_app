3852d21ae06c314915c369ff4087abb64732658c
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2023-12-29 10:43:04 +0700

    updatetable18

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -128,12 +128,6 @@
             </div>
         </div>
 
-        <!--Start Back To Top Button-->
-        <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
-        <!--End Back To Top Button-->
-
-
-
     </div>
     <!--wrapper-->
 
