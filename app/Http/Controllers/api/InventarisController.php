2646e0a1e1f1104ec30791b7445d8622326909ac
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2024-04-05 00:19:31 +0700

    updateapi

---
+++ app/Http/Controllers/api/InventarisController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use Response;
+use Illuminate\Support\Facades\Response;
+use DB;
+class InventarisController extends Controller
+{
+    public function index($id)
+    {
+        try {
+            $category = DB::table('sub_tbl_inventory')->where('kd_cabang',$id)->where('kd_inventaris','05.04.03')->orWhere('kd_inventaris','05.04.04')->get();
+            return response()->json($category);
+        } catch (QueryException $e) {
+            $error = [
+                'error' => $e->getMessage()
+            ];
+            return response()->json($error, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
