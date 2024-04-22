89b610fa6b5bf2f5190064181728b77c4adcc634
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2024-04-22 10:34:46 +0700

    updateklasifikasinamabrang

--- resources/views/faq/dataklasifikasi.blade.php
+++ resources/views/faq/dataklasifikasi.blade.php
@@ -9,7 +9,7 @@
                     $kategori = DB::table('tbl_inventory')->where('no_urut_barang',$item->no_urut_barang)->orderBy('kd_inventaris', 'ASC')->get();
                 @endphp
                 @foreach ($kategori as $itemkategori)
-                <a href="javaScript:void();" class="list-group-item list-group-item-action">{{$itemkategori->kd_inventaris}} : {{$itemkategori->nama_barang}}</a>
+                <a href="javaScript:void();" class="list-group-item list-group-item-action">{{$itemkategori->kd_inventaris}} : {{$itemkategori->nama_klasifikasi_barang}}</a>
                 @endforeach
               </div>
            </div>
