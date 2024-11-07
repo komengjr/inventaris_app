87fc0c8bd8f370344acb47833b70a5224af0724c
Agus Raharjo <agusprasetyoraharjo@gmail.com>
2024-11-07 13:03:50 +0700

    updatekirimnotifikassi10

--- resources/views/divisi/print/peminjaman.blade.php
+++ resources/views/divisi/print/peminjaman.blade.php
@@ -3,10 +3,14 @@
 
 <head>
 
-    <title>Report Peminjaman - {{$datapinjam[0]->pj_pinjam}}</title>
+    <title>Report Peminjaman - {{ $datapinjam[0]->pj_pinjam }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css" integrity="sha512-SgaqKKxJDQ/tAUAAXzvxZz33rmn7leYDYfBP+YoMRSENhf3zJyx3SBASt/OfeQwBHA1nxMis7mM3EV/oYT6Fdw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css"
+        integrity="sha512-SgaqKKxJDQ/tAUAAXzvxZz33rmn7leYDYfBP+YoMRSENhf3zJyx3SBASt/OfeQwBHA1nxMis7mM3EV/oYT6Fdw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"
+        integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <style>
     /* @import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap'); */
@@ -46,6 +50,7 @@
         height: 104px;
         border: 1px solid #252424;
     }
+
     div.absolute-kiri {
         position: absolute;
         top: 0px;
@@ -61,6 +66,7 @@
         margin: 0px;
         font-weight: bold;
     }
+
     div.footer {
         position: fixed;
         left: 0;
@@ -73,16 +79,33 @@
 
 <body style="padding-top: 25px; padding-left: 0px;">
     @php
-        $cabang = DB::table('tbl_cabang')->select('nama_cabang','alamat','city')->where('kd_cabang',$datapinjam[0]->kd_cabang)->get();
-        $staffantarcabang = DB::table('tbl_staff')->select('nama_staff')->where('nip',$datapinjam[0]->pj_pinjam_cabang)->first();
+        $cabang = DB::table('tbl_cabang')
+            ->select('nama_cabang', 'alamat', 'city')
+            ->where('kd_cabang', $datapinjam[0]->kd_cabang)
+            ->get();
+        $staffantarcabang = DB::table('tbl_staff')
+            ->select('nama_staff')
+            ->where('nip', $datapinjam[0]->pj_pinjam_cabang)
+            ->first();
     @endphp
     <div class="header">
         <div class="absolute-kiri">
-            <img style="padding-top: 0px; margin: 2px; left: 2px; ;" src="icon.png" width="152">
+            @php
+                $entitas = DB::table('tbl_cabang')
+                    ->where('kd_cabang', Auth::user()->cabang)
+                    ->first();
+            @endphp
+            @if ($entitas->kd_entitas_cabang == 'PTP')
+                <img style="padding-top: 0px; margin: 2px; left: 2px; ;" src="vendor/pramita.png" width="152">
+            @elseif($entitas->kd_entitas_cabang == 'SIMA')
+                <img style="padding-top: 0px; margin: 2px; left: 2px; ;" src="vendor/sima.jpeg" width="152">
+            @endif
             <hr style="padding: 0%; margin: 0%;">
-            <p style="font-size: 9px; text-align: center; margin-left: 2px;margin-right: 2px;">{{$cabang[0]->alamat}}</p>
+            <p style="font-size: 9px; text-align: center; margin-left: 2px;margin-right: 2px;">{{ $cabang[0]->alamat }}
+            </p>
         </div>
-        <h5 style="padding-top: 20px; margin: 20px; left: 100px; padding-left: 155px;text-decoration: underline;" >FORM PEMAKAIAN BARANG INVENTARIS</h5>
+        <h5 style="padding-top: 20px; margin: 20px; left: 100px; padding-left: 155px;text-decoration: underline;">FORM
+            PEMAKAIAN BARANG INVENTARIS</h5>
         {{-- <img style="padding-top: 11px;" src="data:image/png;base64, {!! base64_encode( QrCode::eyeColor(0, 255, 0, 0, 0, 0, 0)->style('round')->eye('circle')->format('svg')->size(107)->errorCorrection('H')->generate(123123),) !!}"> --}}
 
         <div class="absolute">
@@ -93,9 +116,11 @@
     </div>
     <div class="body">
         <br>
-        <table style="font-size: 8px; margin: 0px; padding: 0px; width: 710px; font-size: 11px; font-family: Calibri (Body);" border="0">
+        <table
+            style="font-size: 8px; margin: 0px; padding: 0px; width: 710px; font-size: 11px; font-family: Calibri (Body);"
+            border="0">
             <tr>
-                <td colspan="3" class="text-right"><strong>SDM.{{$no->no_cabang}}-FRM-PP-07.2/02 </strong></td>
+                <td colspan="3" class="text-right"><strong>SDM.{{ $no->no_cabang }}-FRM-PP-07.2/02 </strong></td>
             </tr>
             <tr>
                 <td colspan="3">
@@ -106,22 +131,24 @@
             <tr>
                 <td style="width: 150px;">Tujuan Peminjaman</td>
                 <td style="width: 5px;">:</td>
-                <td>{{$datapinjam[0]->nama_kegiatan}}</td>
+                <td>{{ $datapinjam[0]->nama_kegiatan }}</td>
             </tr>
             <tr>
                 <td style="width: 150px;">Penanggung Jawab</td>
                 <td style="width: 5px;">:</td>
                 @php
-                    $namapj = DB::table('tbl_staff')->where('nip',$datapinjam[0]->pj_pinjam)->first();
+                    $namapj = DB::table('tbl_staff')
+                        ->where('nip', $datapinjam[0]->pj_pinjam)
+                        ->first();
                 @endphp
-                <td>{{$namapj->nama_staff}}</td>
+                <td>{{ $namapj->nama_staff }}</td>
             </tr>
             <tr>
                 <td>Asal Cabang</td>
                 <td style="width: 5px;">:</td>
                 <td>
 
-                    {{$cabang[0]->nama_cabang}}
+                    {{ $cabang[0]->nama_cabang }}
                 </td>
             </tr>
 
@@ -130,15 +157,20 @@
                 <td>:</td>
                 <td>
                     @php
-                        $cabang1 = DB::table('tbl_cabang')->select('nama_cabang')->where('kd_cabang',$datapinjam[0]->tujuan_cabang)->get();
+                        $cabang1 = DB::table('tbl_cabang')
+                            ->select('nama_cabang')
+                            ->where('kd_cabang', $datapinjam[0]->tujuan_cabang)
+                            ->get();
                     @endphp
-                    {{$cabang1[0]->nama_cabang}}
+                    {{ $cabang1[0]->nama_cabang }}
                 </td>
             </tr>
         </table>
         <br>
-        <table style="font-size: 8px; margin: 0px; padding: 2px; width: 710px; font-size: 11px; font-family: Calibri (Body);" border="1">
-           <thead style="font-weight: bold;">
+        <table
+            style="font-size: 8px; margin: 0px; padding: 2px; width: 710px; font-size: 11px; font-family: Calibri (Body);"
+            border="1">
+            <thead style="font-weight: bold;">
                 <tr>
                     <td class="text-center" rowspan="2">No</td>
                     <td class="text-center" colspan="2">Tanggal</td>
@@ -156,93 +188,93 @@
                     <td class="text-center">Kembali</td>
 
                 </tr>
-           </thead>
-           <tbody>
-            @php
-                $no = 1;
-            @endphp
-            @foreach ($databrg as $item)
+            </thead>
+            <tbody>
+                @php
+                    $no = 1;
+                @endphp
+                @foreach ($databrg as $item)
+                    <tr>
+                        <td class="text-center">{{ $no++ }}</td>
+                        <td>{{ $item->tgl_pinjam_barang }}</td>
+                        <td>{{ $item->tgl_kembali_barang }}</td>
+                        <td>{{ $item->nama_barang }}</td>
+                        <td>
+                            <table border="0" style="font-size: 9px;">
+                                <tr>
+                                    <td>Merek</td>
+                                    <td>:</td>
+                                    <td>{{ $item->merk }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Type</td>
+                                    <td>:</td>
+                                    <td>{{ $item->type }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Seri</td>
+                                    <td>:</td>
+                                    <td>{{ $item->no_seri }}</td>
+                                </tr>
+                            </table>
 
-                <tr>
-                    <td class="text-center">{{$no++}}</td>
-                    <td>{{$item->tgl_pinjam_barang}}</td>
-                    <td>{{$item->tgl_kembali_barang}}</td>
-                    <td>{{$item->nama_barang}}</td>
-                    <td>
-                        <table border="0" style="font-size: 9px;">
-                            <tr>
-                                <td>Merek</td>
-                                <td>:</td>
-                                <td>{{$item->merk}}</td>
-                            </tr>
-                            <tr>
-                                <td>Type</td>
-                                <td>:</td>
-                                <td>{{$item->type}}</td>
-                            </tr>
-                            <tr>
-                                <td>Seri</td>
-                                <td>:</td>
-                                <td>{{$item->no_seri}}</td>
-                            </tr>
-                        </table>
-
-                    </td>
-                    <td>{{ $item->kondisi_pinjam }}</td>
-                    <td>{{ $item->kondisi_kembali }}</td>
-                    <td class="text-center">
-                        @if ($item->status_sub_peminjaman == 0)
-                            {{-- <img style="padding-top: 0px; margin: 2px; left: 2px; ;" src="silang.png" width="15"> --}}
-                            Barang Keluar
-                        @else
-                            <img style="padding-top: 0px; margin: 2px; left: 2px; ;" src="ceklist.png" width="15">
-                        @endif
-                    </td>
-                </tr>
-
-            @endforeach
-           </tbody>
+                        </td>
+                        <td>{{ $item->kondisi_pinjam }}</td>
+                        <td>{{ $item->kondisi_kembali }}</td>
+                        <td class="text-center">
+                            @if ($item->status_sub_peminjaman == 0)
+                                {{-- <img style="padding-top: 0px; margin: 2px; left: 2px; ;" src="silang.png" width="15"> --}}
+                                Barang Keluar
+                            @else
+                                <img style="padding-top: 0px; margin: 2px; left: 2px; ;" src="ceklist.png"
+                                    width="15">
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
         <br><br><br>
 
-    <div class="footer">
-        <table
-            style="font-size: 8px; margin: 0px; padding: 0px; width: 710px; font-size: 11px; font-family: Calibri (Body);"
-            border="1">
-            <tr>
+        <div class="footer">
+            <table
+                style="font-size: 8px; margin: 0px; padding: 0px; width: 710px; font-size: 11px; font-family: Calibri (Body);"
+                border="1">
+                <tr>
 
-                <td colspan="3" class="text-right"><strong>{{$cabang[0]->city}} , {{date('d - m - Y ')}}</strong></td>
-            </tr>
-            <tr>
-                <td>Penanggung Jawab ,</td>
-                <td>Yang Menyerahkan ,</td>
-                <td>Yang Mengetahui ,</td>
-            </tr>
-            <tr >
-                <td class="text-center" style="padding-top: 10px; padding-bottom: 10px; width: 33%;">
-                    {{-- <img style="padding-left: 2px; left: 20px;" src=""> --}}
-                    <br><br><br><br><br>
-                    @if ($staffantarcabang)
-                        {{$staffantarcabang->nama_staff}}
-                    @else
-                        {{$namapj->nama_staff}}
-                    @endif
-
-
-                </td>
-                <td class="text-center" style="width: 33%;">
-                    <br><br><br><br><br>
+                    <td colspan="3" class="text-right"><strong>{{ $cabang[0]->city }} ,
+                            {{ date('d - m - Y ') }}</strong></td>
+                </tr>
+                <tr>
+                    <td>Penanggung Jawab ,</td>
+                    <td>Yang Menyerahkan ,</td>
+                    <td>Yang Mengetahui ,</td>
+                </tr>
+                <tr>
+                    <td class="text-center" style="padding-top: 10px; padding-bottom: 10px; width: 33%;">
+                        {{-- <img style="padding-left: 2px; left: 20px;" src=""> --}}
+                        <br><br><br><br><br>
+                        @if ($staffantarcabang)
+                            {{ $staffantarcabang->nama_staff }}
+                        @else
+                            {{ $namapj->nama_staff }}
+                        @endif
 
 
-                </td>
-                <td class="text-center" style="width: 33%;">
-                    <br><br><br><br><br>
+                    </td>
+                    <td class="text-center" style="width: 33%;">
+                        <br><br><br><br><br>
 
-                </td>
-            </tr>
 
-        </table>
-    </div>
+                    </td>
+                    <td class="text-center" style="width: 33%;">
+                        <br><br><br><br><br>
+
+                    </td>
+                </tr>
+
+            </table>
+        </div>
     </div>
 
 
