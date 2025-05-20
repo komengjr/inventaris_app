c81c14d7bfe5680e3d14ceb5c027f9a2fc1291cc
komengjr <47206605+komengjr@users.noreply.github.com>
2025-05-20 14:49:17 +0700

    updateklasifikasi14

--- resources/views/application/error/404.blade.php
+++ resources/views/application/error/404.blade.php
@@ -86,7 +86,7 @@
                             <hr />
                             <p>Make sure the address is correct and that the page hasn't moved. If you think this is a
                                 mistake, <a href="mailto:info@exmaple.com">contact us</a>.</p><a
-                                class="btn btn-primary btn-sm mt-3" href="../../index.html"><span
+                                class="btn btn-primary btn-sm mt-3" href="{{ url('/', []) }}"><span
                                     class="fas fa-home me-2"></span>Take me home</a>
                         </div>
                     </div>
