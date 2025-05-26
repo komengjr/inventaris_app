2dedcd781763bbe9f84f3783d74c4439ef032ff5
komengjr <47206605+komengjr@users.noreply.github.com>
2025-05-26 10:43:09 +0700

    updatedocument1

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -174,7 +174,7 @@
                                 <li class="nav-item">
                                     <!-- label-->
                                     <div class="row navbar-vertical-label-wrapper mt-3 mb-2">
-                                        <div class="col-auto navbar-vertical-label">Master Data
+                                        <div class="col-auto navbar-vertical-label">Master Admin Data
                                         </div>
                                         <div class="col ps-0">
                                             <hr class="mb-0 navbar-vertical-divider" />
