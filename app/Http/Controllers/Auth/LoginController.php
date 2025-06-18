277e3dc530e58d61482c9546aba35fb3b1af5bd8
komengjr <47206605+komengjr@users.noreply.github.com>
2025-06-18 08:50:57 +0700

    updateselalus2

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,7 +43,7 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            if (Auth::user()->akses == 'dir' || Auth::user()->akses == 'admin' || Auth::user()->akses == 'staff') {
+            if (Auth::user()->akses == 'dir' || Auth::user()->akses == 'admin' || Auth::user()->akses == 'staff' || Auth::user()->akses == 'sdm') {
                 return redirect()->route('dashboard_home')->withSuccess('Kamu Berhasil Masuk di Account  ' . Auth::user()->name);
             } else {
                 return redirect()->intended('home')->withSuccess('Kamu Berhasil Masuk di Account  ' . Auth::user()->name);
