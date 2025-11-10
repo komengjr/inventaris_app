b665552ec45b6ee8b9919627e81f2bbe702b1a20
komengjr <47206605+komengjr@users.noreply.github.com>
2025-11-10 07:32:48 +0700

    updatelogin1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login | Penilaian Supplier</title>
+    <title>Login | Inventaris Managemen System</title>
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/header.png') }}">
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
